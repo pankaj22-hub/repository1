70297e46355d38b1a5ccbf4abacaa68e79a490c9
pankaj22-hub <103204686+pankaj22-hub@users.noreply.github.com>
2022-04-07 21:26:00 +0530

    project

---
+++ Substraction.java
@@ -0,0 +1,13 @@
+
+public class Substraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int a,b,c;
+   a=30;
+   b=21;
+   c=a-b;
+   System.out.println(c);
+	}
+
+}
